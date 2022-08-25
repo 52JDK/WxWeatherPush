8e0c034f1a99acf5ae983a135972b8fded389b1c
xiao-r <shenhao@xueqiu.com>
2022-08-25 10:24:13 +0800

    aa

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,15 +1,9 @@
 package com.example.demo;
 
-import com.example.demo.server.PushServer;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
-import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.scheduling.annotation.EnableScheduling;
-
-import javax.annotation.Resource;
-
 @SpringBootApplication
 @EnableScheduling
 @ConfigurationPropertiesScan
